ae13e13bdd69978a300e20bf004198005ab2f55d
Adoria298 <bananaorange29@outlook.com>
2019-10-08 18:21:31 +0100

    Moved logic in sharedfuncs out of main.go

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
+
+	"github.com/adoria298/guessthenumber/sharedfuncs"
 
 	"github.com/adoria298/guessthenumber/guessing"
 )
@@ -15,16 +12,9 @@ func main() {
 	fmt.Println("Would you like to guess the number, or shall I?")
 	fmt.Println("Input 'Y' if you would, and 'N' if I should.")
 
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSuffix(input, "\r\n")
+	input := sharedfuncs.ReadlnFromStdin("\r\n")
 
 	if input == "Y" {
 		guessing.PlayerGuessMainLoop()
 	}
-}
-
-func generateRandomInteger(min, max int) int {
-	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
 }
