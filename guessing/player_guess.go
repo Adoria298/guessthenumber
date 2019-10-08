91f9ac5f2a91ed51d9fdbcf77c4ed1e5397af9b7
Adoria298 <bananaorange29@outlook.com>
2019-10-08 18:14:21 +0100

    Moved player guessing code to guessing package.

---
+++ guessing/player_guess.go
@@ -0,0 +1,54 @@
+package guessing
+
+// based on freshman.tech/golang-guess/
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// PlayerGuessMainLoop - main loop that allows the player to guess
+func PlayerGuessMainLoop() {
+	fmt.Println("Guess a number between 1 and 100.")
+	fmt.Println("Please input your guess.")
+
+	secretNumber := generateRandomInteger(1, 100)
+
+	var attempts int
+	for {
+		attempts++
+		fmt.Println("Please input your guess.")
+
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\r\n")
+
+		guess, error := strconv.Atoi(input) // convert to integer
+		if error != nil {
+			fmt.Println("Invalid input. Please enter an integer.")
+			fmt.Println(error)
+			continue
+		}
+
+		fmt.Println("Your guess is: ", guess)
+
+		if guess > secretNumber {
+			fmt.Println("Your guess is bigger than the secret number.")
+		} else if guess < secretNumber {
+			fmt.Println("Your guess is smaller than the secret number.")
+		} else {
+			fmt.Println("Correct. Game over.")
+			break
+		}
+	}
+}
+
+func generateRandomInteger(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// based on freshman.tech/golang-guess/
 package main
 
 import (
@@ -6,43 +5,20 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
-	fmt.Println("Guess a number between 1 and 100.")
-	fmt.Println("Please input your guess.")
+	fmt.Println("Would you like to guess the number, or shall I?")
+	fmt.Println("Input 'Y' if you would, and 'N' if I should.")
 
-	secretNumber := generateRandomInteger(1, 100)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSuffix(input, "\r\n")
 
-	var attempts int
-	for {
-		attempts++
-		fmt.Println("Please input your guess.")
-
-		reader := bufio.NewReader(os.Stdin)
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
-
-		guess, error := strconv.Atoi(input) // convert to integer
-		if error != nil {
-			fmt.Println("Invalid input. Please enter an integer.")
-			fmt.Println(error)
-			continue
-		}
-
-		fmt.Println("Your guess is: ", guess)
-
-		if guess > secretNumber {
-			fmt.Println("Your guess is bigger than the secret number.")
-		} else if guess < secretNumber {
-			fmt.Println("Your guess is smaller than the secret number.")
-		} else {
-			fmt.Println("Correct. Game over.")
-			break
-		}
+	if input == "Y" {
+		guessing.PlayerGuessMainLoop
 	}
 }
 
