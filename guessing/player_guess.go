8fd466dbe884978eb71e5d271fae5a0da5538960
Adoria298 <bananaorange29@outlook.com>
2019-10-08 18:23:29 +0100

    Moved logic from guessing/player_guess.go to sharedfuncs.

--- guessing/player_guess.go
+++ guessing/player_guess.go
@@ -3,29 +3,26 @@ package guessing
 // based on freshman.tech/golang-guess/
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"strconv"
-	"strings"
 	"time"
+
+	"github.com/adoria298/guessthenumber/sharedfuncs"
 )
 
 // PlayerGuessMainLoop - main loop that allows the player to guess
 func PlayerGuessMainLoop() {
 	fmt.Println("Guess a number between 1 and 100.")
 
-	secretNumber := generateRandomInteger(1, 100)
+	secretNumber := sharedfuncs.GenerateRandomInteger(1, 100)
 
 	var attempts int
 	for {
 		attempts++
 		fmt.Println("Please input your guess.")
 
-		reader := bufio.NewReader(os.Stdin)
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input := sharedfuncs.ReadlnFromStdin("\r\n")
 
 		guess, error := strconv.Atoi(input) // convert to integer
 		if error != nil {
