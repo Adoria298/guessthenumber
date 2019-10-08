7b50832a0de9e026c1d3c09edc0a62ea63d9108c
Adoria298 <bananaorange29@outlook.com>
2019-10-08 17:52:19 +0100

    Added corrections

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println("Guess a number between 1 and 100.")
 	fmt.Println("Please input your guess.")
+
+	secretNumber := generateRandomInteger(1, 100)
+	fmt.Println("The secret number is: ", secretNumber)
 
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
@@ -24,4 +29,17 @@ func main() {
 	}
 
 	fmt.Println("Your guess is: ", guess)
+
+	if guess > secretNumber {
+		fmt.Println("Your guess is bigger than the secret number.")
+	} else if guess < secretNumber {
+		fmt.Println("Your guess is smalled than the secret number.")
+	} else {
+		fmt.Println("Correct. Game over.")
+	}
+}
+
+func generateRandomInteger(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
 }
