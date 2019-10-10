f5950bc1f1cce79316696a8de9e2500fdda9318f
Adoria298 <bananaorange29@outlook.com>
2019-10-10 17:15:51 +0100

    Add basic AI guessing algorithm

--- guessing/ai_guess.go
+++ guessing/ai_guess.go
@@ -1,12 +1,28 @@
 package guessing
 
 import (
+	"fmt"
+
 	"github.com/adoria298/guessthenumber/sharedfuncs"
 )
 
 // AIGuessMainLoop - main loop for AI version of the guessing game.
 func AIGuessMainLoop() {
-	sharedfuncs.GenerateRandomInteger(1, 100) // just to preserve import
+	fmt.Println("Think of a number between 1 and 100.")
 
-	// TODO (adoria298): implement main loop for an AI version and add it to main.go
+	var attempts int
+	for {
+		attempts++
+		guess := sharedfuncs.GenerateRandomInteger(1, 100)
+		fmt.Println("Is this your number:", guess, "?")
+		fmt.Println("Input 'h' if it is higher than your number,",
+			"'e' if it is equal to it, and 'l' if it is lower.")
+		guessCorrect := sharedfuncs.ReadlnFromStdin("\r\n")
+		if guessCorrect == "e" {
+			fmt.Println("Yay! I got it!")
+			break
+		} else {
+			fmt.Println("Oh well! That's a shame!")
+		}
+	}
 }
