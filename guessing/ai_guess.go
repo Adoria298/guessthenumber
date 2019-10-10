34e78b9359f5785ab0e6aaedd959c6ac7b4cfb55
Adoria298 <bananaorange29@outlook.com>
2019-10-10 17:08:00 +0100

    Add ai_guess.go

---
+++ guessing/ai_guess.go
@@ -0,0 +1,12 @@
+package guessing
+
+import (
+	"github.com/adoria298/guessthenumber/sharedfuncs"
+)
+
+// AIGuessMainLoop - main loop for AI version of the guessing game.
+func AIGuessMainLoop() {
+	sharedfuncs.GenerateRandomInteger(1, 100) // just to preserve import
+
+	// TODO (adoria298): implement main loop for an AI version and add it to main.go
+}
