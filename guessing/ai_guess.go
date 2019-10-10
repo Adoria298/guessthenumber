7dd0dcbd2a49fb56de2e64e33dccd46719500336
Adoria298 <bananaorange29@outlook.com>
2019-10-10 17:26:03 +0100

    Improved guessing algorithm

--- guessing/ai_guess.go
+++ guessing/ai_guess.go
@@ -9,11 +9,13 @@ import (
 // AIGuessMainLoop - main loop for AI version of the guessing game.
 func AIGuessMainLoop() {
 	fmt.Println("Think of a number between 1 and 100.")
+	min := 1
+	max := 100
 
 	var attempts int
 	for {
 		attempts++
-		guess := sharedfuncs.GenerateRandomInteger(1, 100)
+		guess := sharedfuncs.GenerateRandomInteger(min, max)
 		fmt.Println("Is this your number:", guess, "?")
 		fmt.Println("Input 'h' if it is higher than your number,",
 			"'e' if it is equal to it, and 'l' if it is lower.")
@@ -21,6 +23,10 @@ func AIGuessMainLoop() {
 		if guessCorrect == "e" {
 			fmt.Println("Yay! I got it!")
 			break
+		} else if guessCorrect == "l" {
+			min = guess + 1
+		} else if guessCorrect == "h" {
+			max = guess - 1
 		} else {
 			fmt.Println("Oh well! That's a shame!")
 		}
